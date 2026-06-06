358529c431e86e2abce2075bef57610d0911073f
JeanEdwien <jeanedwien@gmail.com>
2026-06-05 20:49:13 -0400

    nuevo

--- grupos/grupo-devops/variables.tf
+++ grupos/grupo-devops/variables.tf
@@ -1,11 +1,11 @@
 variable "repositorios" {
   type        = set(string)
   description = "Lista de repositorios de la capa de Frontend"
-  default     = ["nor-devops"]
+  default     = ["central_devops"]
 }
 
 variable "aprobadores_usuarios" {
   type        = list(number)
   description = "IDs numéricos de GitHub de los líderes de Frontend"
-  default     = [170660482] # Reemplaza con el ID real del usuario de GitHub
+  default     = [93930384] # Reemplaza con el ID real del usuario de GitHub
 }
